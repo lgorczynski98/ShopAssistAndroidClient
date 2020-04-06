e8327e6976117cb3d6ee703a009cdc8fe690f5bc
lgorczynski98 <lgorczynski98@gmail.com>
2020-04-06 15:13:54 +0200

    added scanned image transform and warp

--- app/src/main/java/com/lgorczynski/shopassist/ui/receipt_scanner/ReceiptScannerCameraFragment.java
+++ app/src/main/java/com/lgorczynski/shopassist/ui/receipt_scanner/ReceiptScannerCameraFragment.java
@@ -1,6 +1,9 @@
 package com.lgorczynski.shopassist.ui.receipt_scanner;
 
+import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceView;
@@ -11,16 +14,19 @@ import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.lgorczynski.shopassist.R;
+import com.lgorczynski.shopassist.ui.receipts.ReceiptsViewModel;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -30,8 +36,15 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.Policy;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ReceiptScannerCameraFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -42,8 +55,10 @@ public class ReceiptScannerCameraFragment extends Fragment implements CameraBrid
     private ImageButton captureButton;
     private JavaCameraView javaCameraView;
     private Mat mRGBA;
+    private Mat origin;
     private Mat imgGray;
     private Mat imgCanny;
+    private List<Point> points;
     private BaseLoaderCallback mLoader = new BaseLoaderCallback(getContext()) {
         @Override
         public void onManagerConnected(int status) {
@@ -61,11 +76,13 @@ public class ReceiptScannerCameraFragment extends Fragment implements CameraBrid
 
         View root = inflater.inflate(R.layout.fragment_receipt_scanner_camera, container, false);
         captureButton = root.findViewById(R.id.receipt_scanner_capture_button);
-        captureButton.setOnClickListener(view -> navController.navigate(R.id.action_receiptScannerCameraFragment_to_receiptScannerImagePreviewFragment));
+//        captureButton.setOnClickListener(view -> navController.navigate(R.id.action_receiptScannerCameraFragment_to_receiptScannerImagePreviewFragment));
+        captureButton.setOnClickListener(view -> cropToPoints(points));
 
         javaCameraView = root.findViewById(R.id.java_camera_view);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
+        javaCameraView.setMaxFrameSize(1440, 2560);
         return root;
     }
 
@@ -105,6 +122,7 @@ public class ReceiptScannerCameraFragment extends Fragment implements CameraBrid
     @Override
     public void onCameraViewStopped() {
         mRGBA.release();
+        origin.release();
         imgGray.release();
         imgCanny.release();
     }
@@ -112,19 +130,21 @@ public class ReceiptScannerCameraFragment extends Fragment implements CameraBrid
     @Override
     public void onCameraViewStarted(int width, int height) {
         mRGBA = new Mat(height, width, CvType.CV_8UC4);
+        origin = new Mat(height, width, CvType.CV_8UC4);
         imgGray = new Mat(height, width, CvType.CV_8UC1);
         imgCanny = new Mat(height, width, CvType.CV_8UC1);
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRGBA = inputFrame.rgba();
+        origin = inputFrame.rgba();
+        origin.copyTo(mRGBA);
         try {
             Mat mResizedRGBA = new Mat();
             Imgproc.resize(mRGBA, mResizedRGBA, new Size(mRGBA.cols()/ SCALE_FACTOR, mRGBA.rows()/ SCALE_FACTOR));
 
             List<MatOfPoint> matOfPoints = new ArrayList<>();
-            List<Point> points = getRect(mResizedRGBA).toList();
+            points = getRect(mResizedRGBA).toList();
             points.forEach(point -> {
                 point.x = point.x*SCALE_FACTOR;
                 point.y = point.y*SCALE_FACTOR;
@@ -197,5 +217,100 @@ public class ReceiptScannerCameraFragment extends Fragment implements CameraBrid
         Imgproc.findContours(imgCanny, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
         return getLargestContour(contours);
 
+    }
+
+    private void cropToPoints(List<Point> corners){
+
+        if(corners.size() == 0)
+            return;
+        sortCorners(corners);
+
+        double top = Math.sqrt(Math.pow(corners.get(0).x -
+                corners.get(1).x, 2) + Math.pow(corners.get(0).y -
+                corners.get(1).y, 2));
+        double right = Math.sqrt(Math.pow(corners.get(1).x -
+                corners.get(2).x, 2) + Math.pow(corners.get(1).y -
+                corners.get(2).y, 2));
+        double bottom = Math.sqrt(Math.pow(corners.get(2).x -
+                corners.get(3).x, 2) + Math.pow(corners.get(2).y -
+                corners.get(3).y, 2));
+        double left = Math.sqrt(Math.pow(corners.get(3).x -
+                corners.get(1).x, 2) + Math.pow(corners.get(3).y -
+                corners.get(1).y, 2));
+        Mat quad = Mat.zeros(new Size(Math.max(left, right),
+                Math.max(top, bottom)), CvType.CV_8UC3);
+
+        ArrayList<Point> result_pts = new ArrayList<>();
+        result_pts.add(new Point(quad.cols(), 0));
+        result_pts.add(new Point(0, 0));
+        result_pts.add(new Point(0, quad.rows()));
+        result_pts.add(new Point(quad.cols(), quad.rows()));
+
+        Mat cornerPts = Converters.vector_Point2f_to_Mat(corners);
+        Mat resultPts = Converters.vector_Point2f_to_Mat(result_pts);
+
+        Mat transformation = Imgproc.getPerspectiveTransform(cornerPts, resultPts);
+        Imgproc.warpPerspective(origin, quad, transformation, quad.size());
+        Bitmap bitmap = Bitmap.createBitmap(quad.cols(), quad.rows(),
+                Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(quad, bitmap);
+
+        Bundle bundle = new Bundle();
+        try {
+            File pictureFile = createImageFile();
+            String imagePath = pictureFile.getAbsolutePath();
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.close();
+            bundle.putString("imagePath", imagePath);
+        }
+        catch(Exception e) {
+            Log.d(TAG, "cropToPoints: Error while creating image file");
+        }
+
+        navController.navigate(R.id.action_receiptScannerCameraFragment_to_receiptScannerImagePreviewFragment, bundle);
+    }
+
+    private void sortCorners(List<Point> corners)
+    {
+        List<Point> top = new ArrayList<>();
+        List<Point> bottom = new ArrayList<>();
+        Point center = new Point();
+        for(int i=0; i<corners.size(); i++){
+            center.x += corners.get(i).x/corners.size();
+            center.y += corners.get(i).y/corners.size();
+        }
+
+        for (int i = 0; i < corners.size(); i++)
+        {
+            if (corners.get(i).y < center.y)
+                top.add(corners.get(i));
+            else
+                bottom.add(corners.get(i));
+        }
+        corners = new ArrayList<>();
+        if (top.size() == 2 && bottom.size() == 2){
+            Point top_left = top.get(0).x > top.get(1).x ?
+                    top.get(1) : top.get(0);
+            Point top_right = top.get(0).x > top.get(1).x ?
+                    top.get(0) : top.get(1);
+            Point bottom_left = bottom.get(0).x > bottom.get(1).x
+                    ? bottom.get(1) : bottom.get(0);
+            Point bottom_right = bottom.get(0).x > bottom.get(1).x
+                    ? bottom.get(0) : bottom.get(1);
+
+            corners.add(top_left);
+            corners.add(top_right);
+            corners.add(bottom_right);
+            corners.add(bottom_left);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timestamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        return image;
     }
 }
--- app/src/main/java/com/lgorczynski/shopassist/ui/receipt_scanner/ReceiptScannerImagePreviewFragment.java
+++ app/src/main/java/com/lgorczynski/shopassist/ui/receipt_scanner/ReceiptScannerImagePreviewFragment.java
@@ -1,20 +1,29 @@
 package com.lgorczynski.shopassist.ui.receipt_scanner;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.lgorczynski.shopassist.R;
 
+import java.io.File;
+
+
 public class ReceiptScannerImagePreviewFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
         View root = inflater.inflate(R.layout.fragment_receipt_scanner_preview, container, false);
+
+        final ImageView previewImage = root.findViewById(R.id.receipt_scanner_preview_image);
+        previewImage.setImageURI(Uri.fromFile(new File(getArguments().getString("imagePath"))));
 
         return root;
     }
